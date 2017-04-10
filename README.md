3ec3967f3ec3a11f2173056dc2d0e382f62f4ee8
Paul Okstad <pokstad@smartperimeter.io>
2017-04-09 22:00:47 -0700

    Good enough for the blog piece until feedback.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # anything
 How to use protobuf any.Any
+
+Make sure you are running Go v1.8 or newer
+
+Get the protobuf compiler (v3.2 or newer)
+
+Get the protobuf Go library:
+
+`go get -u github.com/golang/protobuf`
+
+Check out my full write up on Medium: https://medium.com/@pokstad
--- anything_test.go
+++ anything_test.go
@@ -1,19 +1,19 @@
-package anything
+package anything_test
 
 //go:generate protoc --go_out=. anything.proto
 
 import (
-	"log"
-
 	"reflect"
+	"testing"
 
 	proto "github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	any "github.com/golang/protobuf/ptypes/any"
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/pokstad/anything"
 )
 
-func main() {
+func TestAnything(t *testing.T) {
 	t1 := &timestamp.Timestamp{
 		Seconds: 5, // easy to verify
 		Nanos:   6, // easy to verify
@@ -21,14 +21,14 @@ func main() {
 
 	serialized, err := proto.Marshal(t1)
 	if err != nil {
-		log.Fatal("could not serialize timestamp")
+		t.Fatal("could not serialize timestamp")
 	}
 
 	// Blue was a great album by 3EB, before Cadgogan got kicked out
 	// and Jenkins went full primadonna
-	a := AnythingForYou{
+	a := anything.AnythingForYou{
 		Anything: &any.Any{
-			TypeUrl: "google.protobuf.Timestamp",
+			TypeUrl: "example.com/yaddayaddayadda/" + proto.MessageName(t1),
 			Value:   serialized,
 		},
 	}
@@ -36,23 +36,23 @@ func main() {
 	// marshal to simulate going on the wire:
 	serializedA, err := proto.Marshal(&a)
 	if err != nil {
-		log.Fatal("could not serialize anything")
+		t.Fatal("could not serialize anything")
 	}
 
 	// unmarshal to simulate coming off the wire
-	var a2 AnythingForYou
+	var a2 anything.AnythingForYou
 	if err := proto.Unmarshal(serializedA, &a2); err != nil {
-		log.Fatal("could not deserialize anything")
+		t.Fatal("could not deserialize anything")
 	}
 
 	// unmarshal the timestamp
 	var t2 timestamp.Timestamp
 	if err := ptypes.UnmarshalAny(a2.Anything, &t2); err != nil {
-		log.Fatal("Could not unmarshal timestamp from anything field")
+		t.Fatalf("Could not unmarshal timestamp from anything field: %s", err)
 	}
 
 	// Verify the values are as expected
-	if !reflect.DeepEqual(t1, t2) {
-		log.Fatal("Values don't match up")
+	if !reflect.DeepEqual(t1, &t2) {
+		t.Fatalf("Values don't match up:\n %+v \n %+v", t1, t2)
 	}
 }
